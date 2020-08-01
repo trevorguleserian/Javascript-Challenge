8c39ca69d6ebcbd2326d1f062648cef2883843da
Trevor Guleserian <trevorguleserian@gmail.com>
2020-08-01 14:52:58 -0500

    cleaning up code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,6 +10,7 @@ var form = d3.select("#ufo-table");
 // Select the table
 var table = d3.select("tbody")
 
+
 // Creating the table
 tableData.forEach(UFO =>{
   row = table.append("tr")
@@ -17,12 +18,12 @@ tableData.forEach(UFO =>{
     row.append("td").text(value)
   })
 })
-  
 
 
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
+
 
 // Complete the event handler function for the form 
 function runEnter() {
@@ -36,45 +37,20 @@ function runEnter() {
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
- 
+  // Filtering data based on input value
   var filteredData = tableData.filter(UFO => UFO.datetime === inputValue);
 
   
-
+  // Clearing previous table
   table.html("")
 
+  // Creating filtered table
   filteredData.forEach(UFO =>{
     row = table.append("tr")
     Object.entries(UFO).forEach(([key, value])=>{
       row.append("td").text(value)
     })
   });
-
-
 };
 
-//   // BONUS: Calculate summary statistics for the age field of the filtered data
 
-//   // First, create an array with just the age values
-//   var ages = filteredData.map(person => person.age);
-
-//   // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-//   var mean = math.mean(ages);
-//   var median = math.median(ages);
-//   var mode = math.mode(ages);
-//   var variance = math.var(ages);
-//   var standardDeviation = math.std(ages);
-
-//   // Then, select the unordered list element by class name
-//   var list = d3.select(".summary");
-
-//   // remove any children from the list to
-//   list.html("");
-
-//   // append stats to the list
-//   list.append("li").text(`Mean: ${mean}`);
-//   list.append("li").text(`Median: ${median}`);
-//   list.append("li").text(`Mode: ${mode}`);
-//   list.append("li").text(`Variance: ${variance}`);
-//   list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-// };
