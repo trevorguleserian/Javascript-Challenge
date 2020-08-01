b690798f3c34b569ea0355f991e90842eadb741a
Trevor Guleserian <trevorguleserian@gmail.com>
2020-08-01 14:48:05 -0500

    basic challenge complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,80 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#ufo-table");
+
+// Select the table
+var table = d3.select("tbody")
+
+// Creating the table
+tableData.forEach(UFO =>{
+  row = table.append("tr")
+  Object.entries(UFO).forEach(([key, value])=>{
+    row.append("td").text(value)
+  })
+})
+  
+
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form 
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+ 
+  var filteredData = tableData.filter(UFO => UFO.datetime === inputValue);
+
+  
+
+  table.html("")
+
+  filteredData.forEach(UFO =>{
+    row = table.append("tr")
+    Object.entries(UFO).forEach(([key, value])=>{
+      row.append("td").text(value)
+    })
+  });
+
+
+};
+
+//   // BONUS: Calculate summary statistics for the age field of the filtered data
+
+//   // First, create an array with just the age values
+//   var ages = filteredData.map(person => person.age);
+
+//   // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
+//   var mean = math.mean(ages);
+//   var median = math.median(ages);
+//   var mode = math.mode(ages);
+//   var variance = math.var(ages);
+//   var standardDeviation = math.std(ages);
+
+//   // Then, select the unordered list element by class name
+//   var list = d3.select(".summary");
+
+//   // remove any children from the list to
+//   list.html("");
+
+//   // append stats to the list
+//   list.append("li").text(`Mean: ${mean}`);
+//   list.append("li").text(`Median: ${median}`);
+//   list.append("li").text(`Mode: ${mode}`);
+//   list.append("li").text(`Variance: ${variance}`);
+//   list.append("li").text(`Standard Deviation: ${standardDeviation}`);
+// };
